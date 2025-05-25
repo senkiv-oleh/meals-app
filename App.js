914da9a7093acd91151eb05495768a1c43806962
Oleh Senkiv <o.senkiv.v@gmail.com>
2025-05-25 15:37:35 +0300

    Add Drawer navigation and FavoritesScreen; refactor App structure and update dependencies

--- App.js
+++ App.js
@@ -1,13 +1,51 @@
-import { StyleSheet, Button} from 'react-native'
+import { StyleSheet, Button } from 'react-native'
 import { StatusBar } from 'expo-status-bar'
 import { NavigationContainer } from '@react-navigation/native'
 import { createNativeStackNavigator } from '@react-navigation/native-stack'
+import { createDrawerNavigator } from '@react-navigation/drawer'
+import { Ionicons } from '@expo/vector-icons'
 
 import CategoriesScreen from './screens/CategoriesScreen'
 import MealsOverviewScreen from './screens/MealsOverviewScreen'
 import MealDetailScreen from './screens/MealDetailScreen'
+import FavoritesScreen from './screens/FavoritesScreen'
 
 const Stack = createNativeStackNavigator()
+const Drawer = createDrawerNavigator()
+
+const DrawerNavigation = () => (
+  <Drawer.Navigator
+    screenOptions={{
+      headerStyle: { backgroundColor: '#351401' },
+      headerTintColor: 'white',
+      sceneCOntentStyle: { backgroundColor: '#3f2f25' },
+      drawerContentStyle: { backgroundColor: '#351401' },
+      drawerInactiveTintColor: 'white',
+      drawerActiveTintColor: '#351401',
+      drawerActiveBackgroundColor: '#e4baa1'
+    }}
+  >
+    <Drawer.Screen
+      name='Categories'
+      component={CategoriesScreen}
+      options={{
+        title: 'All Categories',
+        drawerIcon: ({ color, size }) => (
+          <Ionicons name='list' color={color} size={size} />
+        )
+      }}
+    />
+    <Drawer.Screen
+      name='Favoirites'
+      component={FavoritesScreen}
+      options={{
+        drawerIcon: ({ color, size }) => (
+          <Ionicons name='star' color={color} size={size} />
+        )
+      }}
+    />
+  </Drawer.Navigator>
+)
 
 export default function App () {
   return (
@@ -22,14 +60,20 @@ export default function App () {
           }}
         >
           <Stack.Screen
-            name='MealsCategories'
-            component={CategoriesScreen}
+            name='All categories'
+            component={DrawerNavigation}
             options={{
-              title: 'All Categories'
+              headerShown: false
             }}
           />
           <Stack.Screen name='MealsOverview' component={MealsOverviewScreen} />
-          <Stack.Screen name='MealDetail' component={MealDetailScreen} />
+          <Stack.Screen
+            name='MealDetail'
+            component={MealDetailScreen}
+            options={{
+              title: 'About the meal'
+            }}
+          />
         </Stack.Navigator>
       </NavigationContainer>
     </>
--- data/dummy-data.js
+++ data/dummy-data.js
@@ -1,5 +1,5 @@
-import Category from '../models/category';
-import Meal from '../models/meal';
+import Category from '../models/category'
+import Meal from '../models/meal'
 
 export const CATEGORIES = [
   new Category('c1', 'Italian', '#f5428d'),
@@ -12,7 +12,7 @@ export const CATEGORIES = [
   new Category('c8', 'Asian', '#b9ffb0'),
   new Category('c9', 'French', '#ffc7ff'),
   new Category('c10', 'Summer', '#47fced')
-];
+]
 
 export const MEALS = [
   new Meal(
@@ -334,4 +334,4 @@ export const MEALS = [
     true,
     true
   )
-];
+]
